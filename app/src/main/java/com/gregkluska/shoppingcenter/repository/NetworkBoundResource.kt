a7c73c9ba0e2ad9ae5e7d6620055e0c3e3415ada
Greg Kluska <kluskagreg@gmail.com>
2020-07-18 15:47:14 +0100

    Edit NetworkBoundResource

--- app/src/main/java/com/gregkluska/shoppingcenter/repository/NetworkBoundResource.kt
+++ app/src/main/java/com/gregkluska/shoppingcenter/repository/NetworkBoundResource.kt
@@ -5,10 +5,7 @@ import androidx.lifecycle.MediatorLiveData
 import com.gregkluska.shoppingcenter.util.GenericApiResponse
 import com.gregkluska.shoppingcenter.util.GenericApiResponse.*
 import com.gregkluska.shoppingcenter.util.Resource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 /**
  * A generic class that can provide a resource backed by both the sqlite database and the network.
@@ -35,7 +32,9 @@ abstract class NetworkBoundResource<CacheObject, RequestObject> {
             result.addSource(dbSource) { data ->
                 result.removeSource(dbSource)
                 if (shouldFetch(data)) {
-                    fetchFromNetwork(dbSource)
+                    CoroutineScope(context).launch{
+                        fetchFromNetwork(dbSource)
+                    }
                 } else {
                     result.addSource(dbSource) { newData ->
                         setValue(Resource.success(newData))
@@ -45,7 +44,7 @@ abstract class NetworkBoundResource<CacheObject, RequestObject> {
         }
     }
 
-    private fun fetchFromNetwork(dbSource: LiveData<CacheObject>) {
+    private suspend fun fetchFromNetwork(dbSource: LiveData<CacheObject>) {
         val apiResponse = createCall()
         // we re-attach dbSource as a new source, it will dispatch its latest value quickly
         result.addSource(dbSource) { newData ->
@@ -56,7 +55,9 @@ abstract class NetworkBoundResource<CacheObject, RequestObject> {
             result.removeSource(dbSource)
             when (response) {
                 is ApiSuccessResponse -> {
-                    //saveCallResult(processResponse(response))
+                    CoroutineScope(Dispatchers.IO).launch {
+                        saveCallResult(response.body)
+                    }
                     // we specially request a new live data,
                     // otherwise we will get immediately last cached value,
                     // which may not be updated with latest results received from network.
